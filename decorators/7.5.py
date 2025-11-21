35841c8409a8bbe5624d38746a2cf3a2eb165c89
abdullahhas <abdullah312003@gmail.com>
2025-11-21 18:27:22 +0500

    decorators from docs

--- decorators/7.5.py
+++ decorators/7.5.py
@@ -16,9 +16,15 @@ def a_new_decorator(a_func):
 
 @a_new_decorator
 def a_function_requiring_decoration():
+    #"""Hey you! Decorate me!"""
     print("I am the function which needs some decoration to remove my foul smell")
 
 a_function_requiring_decoration()
+
+print(a_function_requiring_decoration.__name__)
+# Output: wrapTheFunction
+
+
 
 
 
@@ -35,3 +41,6 @@ a_function_requiring_decoration()
 # #outputs:I am doing some boring work before executing a_func()
 # # I am the function which needs some decoration to remove my foul smell
 # # I am doing some boring work after executing a_func()s
+
+
+
