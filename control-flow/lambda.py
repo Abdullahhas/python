f4a73f114869204d8169ccbe826a3628a430c7f4
abdullahhas <abdullah312003@gmail.com>
2026-07-01 13:23:41 +0500

    lambda function

---
+++ control-flow/lambda.py
@@ -0,0 +1,21 @@
+# A lambda is a small anonymous function.
+
+# Small → usually one line.
+# Anonymous → it has no name.
+
+
+add = lambda a, b: a + b
+
+print(add(2, 3))
+
+
+# syntax
+# lambda parameters: expression
+
+# why use lambda
+
+# Suppose you need a function only once
+
+
+# Limitation
+# Lambda can contain only one expression.
